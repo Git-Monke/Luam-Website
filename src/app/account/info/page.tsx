5596343b1e50dd99b1804855b64b127500ba6cf1
Git-Monke <javelasq2006@gmail.com>
2024-01-15 14:52:51 -0800

    Prevented SSR for certain pages

--- src/app/account/info/page.tsx
+++ src/app/account/info/page.tsx
@@ -28,86 +28,90 @@ const flexedSkeleton = (w: number) => {
 };
 
 export default function Page() {
-  const userContext = useUserContext();
+  if (typeof window !== undefined) {
+    const userContext = useUserContext();
 
-  useEffect(() => {
-    const accessCode = localStorage.getItem("accessCode");
+    useEffect(() => {
+      const accessCode = localStorage.getItem("accessCode");
 
-    if (!accessCode) {
-      return;
-    }
+      if (!accessCode) {
+        return;
+      }
 
-    userContext.startLoading();
-    try {
-      fetch(
-        "https://g06vvsjan9.execute-api.us-west-2.amazonaws.com/main/signup",
-        {
-          method: "POST",
-          headers: {
-            ["X-Code"]: accessCode,
-          },
-          cache: "default",
-        }
-      )
-        .then((res) => {
-          if (!res.ok) {
-            throw new Error("Failed to load resources");
+      userContext.startLoading();
+      try {
+        fetch(
+          "https://g06vvsjan9.execute-api.us-west-2.amazonaws.com/main/signup",
+          {
+            method: "POST",
+            headers: {
+              ["X-Code"]: accessCode,
+            },
+            cache: "default",
           }
+        )
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error("Failed to load resources");
+            }
 
-          return res.json();
-        })
-        .then((body) => {
-          if (body.access_token) {
-            localStorage.setItem("authToken", body.access_token);
-            localStorage.setItem("accessCode", "");
+            return res.json();
+          })
+          .then((body) => {
+            if (body.access_token) {
+              localStorage.setItem("authToken", body.access_token);
+              localStorage.setItem("accessCode", "");
 
-            const user_data = body.user_data as UserResponseData;
-            userContext.signIn({
-              ...user_data,
-              Auth: `Bearer ${body.access_token}`,
-            });
-          }
-        })
-        .catch((err) => {
-          userContext.stopLoading();
-        });
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+              const user_data = body.user_data as UserResponseData;
+              userContext.signIn({
+                ...user_data,
+                Auth: `Bearer ${body.access_token}`,
+              });
+            }
+          })
+          .catch((err) => {
+            userContext.stopLoading();
+          });
+      } catch (err) {
+        console.log(err);
+      }
+    }, []);
 
-  return userContext.loading ? (
-    <div className="flex gap-12">
-      <Skeleton className="w-[100px] h-[100px]"></Skeleton>
-      <div className="grid grid-rows-3 grid-cols-2 gap-x-4 align-bottom">
-        <p className="mr-12 w-full text-right font-semibold">Display Name</p>
-        {flexedSkeleton(12)}
-        <p className="mr-12 w-full text-right font-semibold">User Name</p>
-        {flexedSkeleton(14)}
-        <p className="mr-12 w-full text-right font-semibold">User ID</p>
-        {flexedSkeleton(16)}
-      </div>
-    </div>
-  ) : userContext.userData ? (
-    <div>
+    return userContext.loading ? (
       <div className="flex gap-12">
-        <Image
-          src={userContext.userData.AvatarUrl}
-          alt="Profile"
-          height={100}
-          width={100}
-        ></Image>
-        <div className="grid grid-rows-3 grid-cols-2 gap-x-4">
+        <Skeleton className="w-[100px] h-[100px]"></Skeleton>
+        <div className="grid grid-rows-3 grid-cols-2 gap-x-4 align-bottom">
           <p className="mr-12 w-full text-right font-semibold">Display Name</p>
-          <p>{userContext.userData.Name}</p>
+          {flexedSkeleton(12)}
           <p className="mr-12 w-full text-right font-semibold">User Name</p>
-          <p>{userContext.userData.Login}</p>
+          {flexedSkeleton(14)}
           <p className="mr-12 w-full text-right font-semibold">User ID</p>
-          <p>{userContext.userData.UserID}</p>
+          {flexedSkeleton(16)}
         </div>
       </div>
-    </div>
-  ) : (
-    <h3>Sign in with GitHub to view account information</h3>
-  );
+    ) : userContext.userData ? (
+      <div>
+        <div className="flex gap-12">
+          <Image
+            src={userContext.userData.AvatarUrl}
+            alt="Profile"
+            height={100}
+            width={100}
+          ></Image>
+          <div className="grid grid-rows-3 grid-cols-2 gap-x-4">
+            <p className="mr-12 w-full text-right font-semibold">
+              Display Name
+            </p>
+            <p>{userContext.userData.Name}</p>
+            <p className="mr-12 w-full text-right font-semibold">User Name</p>
+            <p>{userContext.userData.Login}</p>
+            <p className="mr-12 w-full text-right font-semibold">User ID</p>
+            <p>{userContext.userData.UserID}</p>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <h3>Sign in with GitHub to view account information</h3>
+    );
+  }
 }
--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -6,18 +6,20 @@ import { useSearchParams, useRouter } from "next/navigation";
 import { useUserContext } from "@/components/userProvider";
 
 export default function page() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const code = searchParams.get("code");
+  if (typeof window !== "undefined") {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const code = searchParams.get("code");
 
-  const userProvider = useUserContext();
+    const userProvider = useUserContext();
 
-  if (code) {
-    userProvider.startLoading();
-    localStorage.setItem("accessCode", code);
+    if (code) {
+      userProvider.startLoading();
+      localStorage.setItem("accessCode", code);
+    }
+
+    router.push("http://localhost:3000/account/info");
+
+    return <div>{code}</div>;
   }
-
-  router.push("http://localhost:3000/account/info");
-
-  return <div>{code}</div>;
 }
